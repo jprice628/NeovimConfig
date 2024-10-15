5ffbad80af79b5a846142055663b91b034e8f22e
jprice628 <jprice628@hotmail.com>
2024-10-14 22:19:43 -0400

    Added CTRL+P keymap for Telescope's find files feature.

--- init.lua
+++ init.lua
@@ -24,6 +24,7 @@ vim.cmd.colorscheme "catppuccin"
 
 -- Telescope (fuzzy searching)
 local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
