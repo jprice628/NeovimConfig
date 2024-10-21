84e9b19f2a6a0eb81a0deb70b86bba322337f91e
jprice628 <jprice628@hotmail.com>
2024-10-20 20:42:33 -0400

    Replaced double quotes with single quotes.

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,17 +1,14 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
     dependencies = {
-        "nvim-lua/plenary.nvim",
-        -- not strictly required, but recommended
-        "nvim-tree/nvim-web-devicons", 
-        "MunifTanjim/nui.nvim",
-        -- "3rd/image.nvim", 
-        -- Optional image support in preview window: See `# Preview Mode` for more information
+        'nvim-lua/plenary.nvim',
+        'nvim-tree/nvim-web-devicons',
+        'MunifTanjim/nui.nvim',
     },
     config = function()
-        vim.keymap.set('n', '<C-n>', 
-            ':Neotree reveal filesystem left<CR>', 
+        vim.keymap.set('n', '<C-n>',
+            ':Neotree reveal filesystem left<CR>',
             { desc = 'Show filesystem tree.' })
     end
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,13 @@
 return {
-    "nvim-treesitter/nvim-treesitter", 
-    build = ":TSUpdate",
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
     config = function()
         require 'nvim-treesitter.install'.prefer_git = false
-        require 'nvim-treesitter.install'.compilers = { "zig" }
-        local config = require("nvim-treesitter.configs")
+        require 'nvim-treesitter.install'.compilers = { 'zig' }
+        local config = require('nvim-treesitter.configs')
         config.setup({
-            ensure_installed = { "c_sharp", "css", "html", "javascript", "lua", 
-                "json", "powershell", "gitignore" },
+            ensure_installed = { 'c_sharp', 'css', 'html', 'javascript', 'lua',
+                'json', 'powershell', 'gitignore' },
             highlight = { enable = true },
             indent = { enable = true }
         })
