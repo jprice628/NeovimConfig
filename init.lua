c973e8d6e270f226bf6e4de63210747b1ffe7bea
jprice628 <jprice628@hotmail.com>
2025-01-11 22:07:37 -0500

    Added window and text block keymaps.

--- init.lua
+++ init.lua
@@ -16,5 +16,17 @@ vim.o.shiftwidth = 4
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+-- Map the window keys
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
+
+-- Map text block movement keys
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
 -- Lazy plugin manager
 require("config.lazy")
